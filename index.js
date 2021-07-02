2db5805a9b1fb767a67e6091f682f25893bf5e59
Phaneendrakumar9 <phaneendrakumar116@gmail.com>
2021-07-02 11:11:16 +0530

    PULL REQUEST

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-'use strict'
-
-const person=function(firstName,birthYear) {
- this.firstName=firstName;
- this.birthYear=birthYear;
+function x(){
+    for(let i=0;i<=6;i++)
+    {
+     function close(i){
+     setTimeout(function(){
+     console.log(i);
+    },i * 1000);
 }
-
-const phani=new person('Phaneendra',2001);
-console.log(phani);
-const yaswanth=new person('Yaswanth',2001);
-console.log(yaswanth);
-
-person.prototype.calcAge=function()
-{
-    console.log(2050-this.birthYear);
+close(i);
 }
-phani.calcAge();
-console.log(phani.__proto__);
-console.log(phani.hasOwnProperty('firstName'));
-console.log(person.prototype.isPrototypeOf(phani));
-
-
-class person{
-    constructor(firstName,birthYear){
-        this.firstName=firstName;
-        this.birthYear=birthYear;
-    }
-    // calcAge(){
-    //    console.log(2050-this.birthYear);
-    // }
+    console.log("Bye");
 }
-
-const phani=new person('Phaneendra',2001);
-console.log(phani);
-
-person.prototype.calcAge=function(){
-    console.log(2050-this.birthYear);
-}
-
-phani.calcAge();
-
+x();
